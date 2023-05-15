e9b8a28d556e622beb4d183dc27645de424054b6
Sofia Chernikova <sofiachern2003@yandex.ru>
2023-05-15 09:03:35 +0300

    #98 feature(backend) update tables

--- backend/src/main/java/ru/fbtw/thestore/backend/controllers/ResponseBuilder.java
+++ backend/src/main/java/ru/fbtw/thestore/backend/controllers/ResponseBuilder.java
@@ -2,7 +2,6 @@ package ru.fbtw.thestore.backend.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import ru.fbtw.thestore.backend.models.DTO.PagingDto;
 
 import java.util.List;
 import java.util.Map;
@@ -16,10 +15,4 @@ public class ResponseBuilder {
         return ResponseEntity.status(httpStatus).body(data);
     }
 
-    public static ResponseEntity<?> build(PagingDto pagingDto, List<?> data) {
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-                "paging", pagingDto,
-                "data", data
-        ));
-    }
 }
