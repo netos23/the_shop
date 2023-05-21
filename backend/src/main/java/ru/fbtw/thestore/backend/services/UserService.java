9e93cd84b4c1701bd791d983d3e870fa0e587fe1
netos23 <nikmoroz2002@outlook.com>
2023-05-21 21:41:53 +0300

    #69 feature(backend) fix security rules

--- backend/src/main/java/ru/fbtw/thestore/backend/services/UserService.java
+++ backend/src/main/java/ru/fbtw/thestore/backend/services/UserService.java
@@ -1,13 +1,21 @@
 package ru.fbtw.thestore.backend.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.fbtw.thestore.backend.domain.user.MyUser;
 import ru.fbtw.thestore.backend.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
-private final UserRepository userRepository;
+public class UserService implements UserDetailsService {
+	private final UserRepository userRepository;
 
+	@Override
+	public MyUser loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+	}
 }
