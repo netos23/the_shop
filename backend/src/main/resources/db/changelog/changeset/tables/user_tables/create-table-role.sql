5c9b2c604229f38562bafb83d2e80a92937e3af1
Sofia Chernikova <sofiachern2003@yandex.ru>
2023-06-04 19:21:34 +0300

    #105 feature(backend) changed database tables

--- backend/src/main/resources/db/changelog/changeset/tables/user_tables/create-table-role.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS my_role
-(
-    role_id INT PRIMARY KEY,
-    role_name VARCHAR(63) NOT NULL
-    );
-
-INSERT INTO my_role (role_id, role_name) VALUES
-                                          (1, 'ADMIN'),
-                                          (2, 'AUTHORIZED_USER');
